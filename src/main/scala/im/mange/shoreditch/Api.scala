ba91f8c3a44aebafd7adcaf2047e29ccce7e7682
Paul Allton <paulallton@gmail.com>
2014-07-12 00:41:46 +0100

    Update Api.scala

--- src/main/scala/im/mange/shoreditch/Api.scala
+++ src/main/scala/im/mange/shoreditch/Api.scala
@@ -2,3 +2,15 @@ package im.mange.shoreditch
 
 case class ActionResponse(failures: List[String], returnValue: Option[String])
 case class CheckResponse(failures: List[String])
+
+trait Action {
+  def run: ActionResponse
+  def success(returnValue: String) = ActionResponse(Nil, Some(returnValue))
+  def failure(failures: List[String]) = ActionResponse(failures, None)
+}
+
+trait Check {
+  def run: CheckResponse
+  def success = CheckResponse(Nil)
+  def failure(failures: List[String]) = CheckResponse(failures)
+}
