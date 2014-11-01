73503ccd8c4e25e1a9d0e28bf0398365aedb8e60
alltonp <paulallton@gmail.com>
2014-11-01 22:21:18 +0000

    try to go back to bintray

--- publish/bintray.sh
+++ publish/bintray.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.bintray
 eval "echo \"$(< ./publish/bintray.template)\"" > ~/.bintray/.credentials
-eval "echo \"$(< ./publish/sonatype.template)\"" > ../sonatype.sbt
+#eval "echo \"$(< ./publish/sonatype.template)\"" > ../sonatype.sbt
 #openssl des3 -d -salt -in ./publish/sonatype.asc.enc -out ./publish/sonatype.asc -k "$SECRET"
