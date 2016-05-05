eb3789d6d505814787442ac1a8df32b9e489e25b
alltonp <paulallton@gmail.com>
2016-05-05 10:04:46 +0100

    add longName and alias

--- src/main/scala/im/mange/shoreditch/api/MetaDataResponse.scala
+++ src/main/scala/im/mange/shoreditch/api/MetaDataResponse.scala
@@ -2,4 +2,4 @@ package im.mange.shoreditch.api
 
 case class CheckMetaData(url: String)
 case class ActionMetaData(url: String, in: List[In], out: Option[Out]) //TODO: Seq
-case class MetaDataResponse(version: String, checks: Seq[CheckMetaData], actions: Seq[ActionMetaData])
+case class MetaDataResponse(longName: String, alias: String, version: String, checks: Seq[CheckMetaData], actions: Seq[ActionMetaData])
